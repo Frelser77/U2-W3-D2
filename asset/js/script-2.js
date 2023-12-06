776eedf1131897161ebc386de712675bb7b362f0
Frelser77 <damicosalvatorealessandro@gmail.com>
2023-12-06 12:36:55 +0100

    commit

--- asset/js/script-2.js
+++ asset/js/script-2.js
@@ -29,10 +29,20 @@ const renderList = function () {
 	petList.innerHTML = "";
 	pets.forEach((pet) => {
 		const newLi = document.createElement("li");
-		newLi.innerText = "Nome: " + pet.petName + ", proprietario: " + pet.ownerName;
+		newLi.classList.add("list-group-item");
+		newLi.style.marginBlock = "8px";
+		newLi.style.borderRadius = "10px";
+		newLi.innerText =
+			"Nome: " + pet.petName + ", Proprietario: " + pet.ownerName + ", Specie: " + pet.species + ", Breed:" + pet.breed;
 		petList.appendChild(newLi);
 	});
 };
+
+pets.push(new Pet("Fido", "Marco", "Cane", "Labrador"));
+pets.push(new Pet("Whiskers", "Sofia", "Gatto", "Siamese"));
+pets.push(new Pet("Buddy", "Luca", "Coniglio", "Olandese"));
+pets.push(new Pet("Goldie", "Giulia", "Pesce", "Goldfish"));
+pets.push(new Pet("Charlie", "Marco", "Pappagallo", "Cacatua"));
 
 addButton.onclick = function () {
 	let newPet = new Pet(petName.value, ownerName.value, species.value, breed.value);
@@ -43,3 +53,5 @@ addButton.onclick = function () {
 	species.value = "";
 	breed.value = "";
 };
+
+console.log(pets);
